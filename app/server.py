0b99ece40a32b64ca3c76d028c893b52160b7054
unknown <daniel.kono@gmail.com>
2020-03-04 19:18:34 -0300

    classifying tom and logan pictures

--- app/server.py
+++ app/server.py
@@ -10,10 +10,10 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1Zct0FBqVYow4hAjqYo1uEFXCWkX4bJmN'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1X6EZFXl6GE1OxwmzKJCJh3ENXbEJvZBg'
 export_file_name = 'export.pkl'
 
-classes = ['danfe', 'darf']
+classes = ['logan', 'tom']
 path = Path(__file__).parent
 
 app = Starlette()
